63ced3a816c8d8cbfa143bed308ca3baa49e25ef
adarshgupta404 <gadarsh747@gmail.com>
2022-09-28 04:22:21 +0530

    commit

---
+++ src/components/SIgn_img.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SIgn_img = () => {
+    return (
+        <>
+            <div className="right_data mt-5" style={{ width: "100%" }}>
+                <div className="sign_img mt-5">
+                    <img src="https://img.freepik.com/free-vector/functional-testing-banner-methodology-programming_107791-3013.jpg?w=1380&t=st=1664302800~exp=1664303400~hmac=044db42b33e3965d1b48b32cd3e7feb70ae6020d8ee3e7be8fc5fc2196ef45ac" style={{ maxWidth: 800 }} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SIgn_img
